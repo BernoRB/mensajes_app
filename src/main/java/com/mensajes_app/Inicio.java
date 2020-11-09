cc7d0e3cb292462aa31957d3a73dfb0687668651
Bernaberb <bernabrb@gmail.com>
2020-11-09 13:08:14 -0300

    commit

---
+++ src/main/java/com/mensajes_app/Inicio.java
@@ -0,0 +1,21 @@
+package com.mensajes_app;
+
+import java.sql.Connection;
+
+/**
+ * Hello world!
+ *
+ */
+public class Inicio 
+{
+    public static void main( String[] args ) {
+    	Conexion conexion = new Conexion();
+    	
+    	try(Connection cnx = conexion.get_connection()){
+    		
+    	}catch(Exception e) {
+    		System.out.println(e);
+    	}
+    	
+    }
+}
